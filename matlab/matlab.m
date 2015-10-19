fe00f78ec8f365b65817f2fb3ce38cb464fe20fe
casper666 <lbjyy@hotmail.com>
2015-10-19 14:20:52 -0400

    add exp4

--- matlab/matlab.m
+++ matlab/matlab.m
@@ -1,7 +1,20 @@
-audiodata1 = csvread('outputfile1.csv');
-audiodata2 = csvread('outputfile2.csv');
+audiodata1 = csvread('./test4/outputfile1.csv');
+audiodata2 = csvread('./test4/outputfile2.csv');
+audiodata3 = csvread('./test4/outputfile3.csv');
+audiodata4 = csvread('./test4/outputfile4.csv');
+audiodata5 = csvread('./test4/outputfile5.csv');
+audiodata6 = csvread('./test4/outputfile6.csv');
 % player = audioplayer(audiodata, 44100);
 % playblocking(player);
 plot(audiodata1);
-%plot(audiodata2);
+figure
+plot(audiodata2);
+figure
+plot(audiodata3);
+figure
+plot(audiodata4);
+figure
+plot(audiodata5);
+figure
+plot(audiodata6);
 % plot(audiodata2 - audiodata1);
---
+++ matlab/test4/readme.md
@@ -0,0 +1,2 @@
+1-3 is hand-based no-wall sound
+4-6 is hand-based 10cm-wall sound

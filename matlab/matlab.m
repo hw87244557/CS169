0cab39908275d5faef372ff6ac8693c420dae77b
casper666 <lbjyy@hotmail.com>
2015-10-18 23:53:13 -0400

    initial results in

--- matlab/matlab.m
+++ matlab/matlab.m
@@ -1,3 +1,5 @@
-audiodata = fread(fopen('recording.pcm'), inf, 'short');
-player = audioplayer(audiodata, 44100);
-playblocking(player);
+% audiodata = fread(fopen('recording2.pcm'), inf, 'short');
+% player = audioplayer(audiodata, 44100);
+% playblocking(player);
+
+plot(audiodata)
